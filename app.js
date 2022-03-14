935b24c697b26b007678d87ceec6217e5ccd0fa8
Turki767 <62534076+TurkiPro@users.noreply.github.com>
2022-03-14 19:50:18 +0300

    Many additions to JS and CSS
    CSS:
    
    - Styled the tiles in CSS so they can appear clearly when testing JS
    
    JS:
    
    - Created the 4 tetris shapes that are needed for the game
    
    - Created two functions for adding the shapes and removing them from display
    
    - Created a function to move the shape across the tiles for the game
    
    - Made the first shape in rotation be chosen randomly
    
    - initialized variables scoreDisplay & startBtn to be used later in development

--- app.js
+++ app.js
@@ -1,3 +1,78 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-})
+    const width = 10;
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#startBtn')
+    const tile = document.querySelector('#tile');
+    let squares = Array.from(document.querySelectorAll('#tile div'));
+
+    //Creating the shapes using arrays
+    const tShape = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+    ];
+
+    const sqrShape = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
+    ];
+
+    const iShape = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
+    ];
+
+    const zShape = [
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1]
+    ];
+
+    const lShape = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ];
+
+    const shapesArray = [tShape, sqrShape, iShape, zShape, lShape];
+
+    let currentPosition = 4;
+    let currentRotation = 0;
+
+    //Randomly choose shapes recursively
+    let random = Math.floor(Math.random() * shapesArray.length);
+    let current = shapesArray[random][currentRotation];
+
+    //draw the shape in rotation
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('shape');
+        })
+    }
+
+    //remove or undraw the shape in rotation
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('shape');
+        })
+    };
+
+    //Moving the shapes across the tiles every second
+    timerId = setInterval(moveDown, 1000);
+
+    function moveDown() {
+        undraw();
+        currentPosition += width;
+        draw();
+    }
+
+
+});
